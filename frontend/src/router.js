503e0d32c4bb2f5d23ab2e848707afb14000d73a
liveangela <cs198500@126.com>
2018-09-09 22:02:27 +0800

    [+]Frontend: set routers

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,26 +1,66 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Store from './store'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const pathListWithoutBottomNav = ['newrole']
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      beforeEnter: (to, from, next) => {
+        if (!Store.state.activeRole) {
+          next('/newrole')
+        } else {
+          next()
+        }
+      }
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/newrole',
+      name: 'newrole',
+      component: () =>
+        import(/* webpackChunkName: "createRole" */ './views/CreateRole.vue'),
+      beforeEnter: (to, from, next) => {
+        if (Store.state.activeRole) {
+          next('/')
+        } else {
+          next()
+        }
+      }
+    },
+    {
+      path: '/error/:type',
+      name: 'error',
+      props: true,
+      component: () =>
+        import(/* webpackChunkName: "error" */ './views/Error.vue')
+    },
+    {
+      path: '*',
+      redirect: '/error/404'
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // bottomNav control
+  const isBottomNavShown = !pathListWithoutBottomNav.includes(to.name || '')
+  Store.dispatch('setBottomNavShown', isBottomNavShown)
+  next()
+})
+
+export default router

a23e46699cfc2ad28ce8c2a8f1d0139dd580e977
liveangela <cs198500@126.com>
2018-09-09 21:57:17 +0800

    [+]Frontend: vuex init and vuex-persist

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    activeRole: null,
+    isBottomNavShown: true
+  },
+  mutations: {
+    setActiveRole(state, val) {
+      state.activeRole = val
+    },
+    setBottomNavShown(state, val) {
+      state.isBottomNavShown = val
+    }
+  },
+  actions: {
+    setActiveRole({commit}, val) {
+      commit('setActiveRole', val)
+    },
+    setBottomNavShown({commit}, val) {
+      commit('setBottomNavShown', val)
+    }
+  },
+  plugins: [new VuexPersistence().plugin]
 })
